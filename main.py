4ecf4ba54c1195fe7c6dca9dbe43b0e5fdc770ae
이주원 <60950583+Su-per@users.noreply.github.com>
2022-12-20 09:19:17 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import httpx
+
+url = "https://port-0-kschool-backend-37y7e24l7jiwra5.gksl1.cloudtype.app/pop/?count=50&token=&schoolCode=7380292"
+
+while True:
+    r = httpx.post(url)
+    r = r.json()
+    try:
+        if r['error'] == True:
+            print("Too many reqeust")
+    except:
+        print(f"랭킹 : {r['rank']} 개수 : {r['pop']}")
