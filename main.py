a552a8721965f19c382fb803cd744b4b32e919d6
이주원 <60950583+Su-per@users.noreply.github.com>
2022-12-20 10:09:37 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
+import os
+os.system("pip3 install httpx")
 import httpx
 
 url = "https://port-0-kschool-backend-37y7e24l7jiwra5.gksl1.cloudtype.app/pop/?count=50&token=&schoolCode=7380292"
 
 while True:
-    r = httpx.post(url)
-    r = r.json()
+    res = httpx.post(url).json()
     try:
-        if r['error'] == True:
+        if res['error'] == True:
             print("Too many reqeust")
     except:
-        print(f"랭킹 : {r['rank']} 개수 : {r['pop']}")
+        print(f"랭킹 : {res['rank']} 개수 : {res['pop']}")
